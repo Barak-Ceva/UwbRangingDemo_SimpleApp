4ab1c214b4b243e2f60da91c4ef6f152e1804d56
Barak-Ceva <barak.roth@ceva-dsp.com>
2024-09-09 11:39:47 +0300

    support both controller and controlee

--- app/src/main/java/com/example/uwbdemoapp/MainActivity.java
+++ app/src/main/java/com/example/uwbdemoapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.uwbdemoapp;
 
-import com.example.uwbdemoapp.R;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -27,6 +25,7 @@ import androidx.core.uwb.UwbManager;
 import androidx.core.uwb.rxjava3.UwbClientSessionScopeRx;
 import androidx.core.uwb.rxjava3.UwbManagerRx;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -44,6 +43,15 @@ public class MainActivity extends AppCompatActivity {
     private static final int SESSION_ID = 0x0A0A0A0A;
     private static final String DEVICE_ADDRESS_STR = "E0:E0";
     public static final byte[] SESSION_KEY = new byte[]{1, 1, 1, 1, 1, 1, 1, 1};
+
+    private static final int NUM_MEASUREMENTS = 10;
+    private static final double[] distanceArray = new double[NUM_MEASUREMENTS];
+    private static int distanceIndex = 0;
+    private static final int DISTANCE_HYSTERESIS = 10;
+
+    private static final double[] azimuthArray = new double[NUM_MEASUREMENTS];
+    private static final double[] elevationArray = new double[NUM_MEASUREMENTS];
+
 
 
 
@@ -63,7 +71,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button initRangingButton = findViewById(R.id.get_values_button);
         Button startRangingButton = findViewById(R.id.communicate_button);
-        Switch isControllerSwitch = findViewById(R.id.is_controller);
+        Switch roleSwitch = findViewById(R.id.is_controller);
+        TextView role = findViewById(R.id.role_text_view);
 
         TextView distanceDisplay = findViewById(R.id.distance_display);
         TextView elevationDisplay = findViewById(R.id.elevation_display);
@@ -73,29 +82,50 @@ public class MainActivity extends AppCompatActivity {
         new Thread(() -> {
             AtomicReference<UwbClientSessionScope> currentUwbSessionScope = new AtomicReference<>(UwbManagerRx.controleeSessionScopeSingle(uwbManager).blockingGet());
 
+
+            roleSwitch.setOnClickListener((v -> {
+                if(roleSwitch.isChecked()){
+                    role.setText("Controller");
+                }
+                else
+                {
+                    role.setText("Controlee");
+                }
+            }));
+            roleSwitch.callOnClick();
+
             initRangingButton.setOnClickListener((view) -> {
                 if (rangingResultObservable.get() != null) {
                     rangingResultObservable.get().dispose();
                     rangingResultObservable.set(null);
                 }
 
-                currentUwbSessionScope.set(UwbManagerRx.controllerSessionScopeSingle(uwbManager).blockingGet());
-                if (isControllerSwitch.isChecked()) {
+                if (roleSwitch.isChecked()) {
                     // CONTROLLER
+                    currentUwbSessionScope.set(UwbManagerRx.controllerSessionScopeSingle(uwbManager).blockingGet());
                     UwbControllerSessionScope controllerSessionScope = (UwbControllerSessionScope) currentUwbSessionScope.get();
-                    MacAddressAlertDialog(view, controllerSessionScope.getLocalAddress().getAddress());
+//                    Log.d(TAG,"Ranging capabilities:isAzimuthalAngleSupported-" + controllerSessionScope.getRangingCapabilities().isAzimuthalAngleSupported());
+//                    Log.d(TAG,"Ranging capabilities:isDistanceSupported-" + controllerSessionScope.getRangingCapabilities().isDistanceSupported());
+//                    Log.d(TAG,"Ranging capabilities:isElevationAngleSupported-" + controllerSessionScope.getRangingCapabilities().isElevationAngleSupported());
+                    MacAddressAlertDialog(view, controllerSessionScope.getLocalAddress().getAddress(), "Controller");
                 } else {
                     // CONTROLLEE;
+                    currentUwbSessionScope.set(UwbManagerRx.controleeSessionScopeSingle(uwbManager).blockingGet());
                     UwbControleeSessionScope controleeSessionScope = (UwbControleeSessionScope) currentUwbSessionScope.get();
-                    MacAddressAlertDialog(view, controleeSessionScope.getLocalAddress().getAddress());
+                    MacAddressAlertDialog(view, controleeSessionScope.getLocalAddress().getAddress(), "Controlee");
                 }
             });
 
             startRangingButton.setOnClickListener((view -> {
                 try {
-                    if (isControllerSwitch.isChecked())
-                        //CONTROLLER
-                        rangingResultObservable.get();
+//                    if (isControllerSwitch.isChecked()) {
+//                        //CONTROLLER
+//                        rangingResultObservable.get();
+//                    }
+//                    else{
+//                        int channelPreamble = Integer.parseInt(preambleInputField.getText().toString());
+//                        uwbComplexChannel = new UwbComplexChannel(9, channelPreamble);
+//                    }
 
                     UwbComplexChannel uwbComplexChannel = new UwbComplexChannel(UWB_CHANNEL, UWB_PREAMBLE_INDEX);
                     UwbDevice uwbDevice = new UwbDevice(new UwbAddress(DEVICE_ADDRESS_STR));
@@ -117,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
 
                                     // Display dialog to inform about lost connection
                                     new AlertDialog.Builder(
-                                            view.getContext()).setTitle("CONTROLLER / SERVER").
+                                            view.getContext()).setTitle("Controller").
                                             setMessage("Connection lost..").setNeutralButton("OK", (a, b) -> {}).create().show();
 
                                 }
@@ -135,22 +165,56 @@ public class MainActivity extends AppCompatActivity {
 
     private static void handleRangingResult(RangingResult.RangingResultPosition rangingResult, TextView distanceDisplay, TextView azimuthDisplay, TextView elevationDisplay) {
         if (rangingResult.getPosition().getDistance() != null) {
-            distanceDisplay.setText(String.valueOf((Float.parseFloat(distanceDisplay.getText().toString()) + rangingResult.getPosition().getDistance().getValue()) / 2));
-            Log.d(TAG,"Distance: " + rangingResult.getPosition().getDistance().getValue());
+
+            double distance = rangingResult.getPosition().getDistance().getValue();
+            distance = movingAverage(distanceArray, distance, distanceIndex);
+            distanceIndex++;
+            String distanceString = String.format("%.2f", distance);
+            distanceDisplay.setText(distanceString);
+            Log.d(TAG,"Distance: " + distanceString);
         }
         if (rangingResult.getPosition().getAzimuth() != null) {
-            azimuthDisplay.setText(String.valueOf((Float.parseFloat(azimuthDisplay.getText().toString()) + rangingResult.getPosition().getAzimuth().getValue()) / 2));
-            Log.d(TAG,"Azimuth: " + rangingResult.getPosition().getAzimuth().getValue());
+            String azimuthString = String.valueOf(rangingResult.getPosition().getAzimuth().getValue());
+            azimuthDisplay.setText(azimuthString);
+            Log.d(TAG,"Azimuth: " + azimuthString);
         }
         if (rangingResult.getPosition().getElevation() != null) {
-            elevationDisplay.setText(String.valueOf((Float.parseFloat(elevationDisplay.getText().toString()) + rangingResult.getPosition().getElevation().getValue()) / 2));
-            Log.d(TAG,"Elevation: " + rangingResult.getPosition().getElevation().getValue());
+            String elevationString = String.valueOf(rangingResult.getPosition().getElevation().getValue());
+            elevationDisplay.setText(elevationString);
+            Log.d(TAG,"Elevation: " + elevationString);
         }
     }
+    public static double movingAverage(double[] arr, double newMeas, int index) {
 
-    private void MacAddressAlertDialog(View view, byte[] macAddress){
+        // Check if the new measurement is too far from the current average
+        double currentAverage = getArrayAverage(arr);
+        if(Math.abs(currentAverage - newMeas) > DISTANCE_HYSTERESIS){
+            newMeas = currentAverage;
+        }
+
+        // Handle the case when the array is not full
+        if(index == 0){
+            Arrays.fill(arr, newMeas);
+            return newMeas;
+        }
+        else {
+            arr[index % arr.length] = newMeas;
+        }
+
+        return getArrayAverage(arr);
+    }
+
+    private static double getArrayAverage(double[] arr) {
+        double sum = 0;
+        for (double i : arr) {
+            sum += i;
+        }
+        return sum / arr.length;
+    }
+
+    private void MacAddressAlertDialog(View view, byte[] macAddress, String role){
         new AlertDialog.Builder(
-                view.getContext()).setTitle("CONTROLLER").
+                view.getContext()).setTitle(role).
                 setMessage("Your MAC Address is: " + toHexLittleEndian(macAddress))
                 .setNeutralButton("OK", (a, b) -> {
                 }).create().show();
